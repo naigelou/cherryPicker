a45164dbe1e11a5cc4ee34a6490d50d379e90560
naigelou <93379182+naigelou@users.noreply.github.com>
2021-12-02 23:13:44 +0200

    Eka alustus

---
+++ src/Cherrypicker/Main.java
@@ -0,0 +1,66 @@
+package Cherrypicker;
+import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.model.RS2Object;
+import org.osbot.rs07.api.ui.Message;
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+import org.osbot.rs07.utility.ConditionalSleep;
+
+@ScriptManifest(name="Cherrypicker", author = "Niko", logo="", version = 1.0, info = "Joku yritys tehä scriptiä")
+public class Main extends Script{
+
+    private Area cherry = new Area(3277, 3360, 3269, 3374);
+    private Area puska1 = new Area(3262, 3363, 3266, 3366);
+
+
+
+    public void onMessage(Message message) throws java.lang.InterruptedException {
+        String txt = message.getMessage().toLowerCase();
+        if (txt.contains("there are no berries on this bush at the moment.")) {
+            log("MENIS LIKKUU JOS OSAIS KOODATA.");
+        }
+    }
+
+
+
+
+
+
+    @Override
+            public int onLoop () throws InterruptedException {
+                if (!cherry.contains(myPlayer())) {
+                    if (getWalking().webWalk(cherry)) {
+                        new ConditionalSleep(1800, 6000) {
+                            @Override
+                            public boolean condition() throws InterruptedException {
+                                return false;
+                            }
+                        }.sleep();
+                    }
+                } else {
+                    RS2Object bush = getObjects().closest("Cadava bush");
+                    if (bush != null) {
+                        if (!myPlayer().isAnimating()) {
+                            bush.interact("Pick-from");
+                            }
+
+                            getMouse().moveOutsideScreen();
+
+                            sleep(random(2000, 4000));
+
+
+
+
+                        }
+
+                    }
+        return 602;
+    }
+
+
+
+
+    }
+
+
+
